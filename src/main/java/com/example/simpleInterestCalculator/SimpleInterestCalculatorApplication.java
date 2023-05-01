120efc94265efc79c06fc7caffdfe52c58203acb
smathur93 <shreymathur93@gmail.com>
2023-05-01 15:00:51 +0530

    phase 1

--- src/main/java/com/example/simpleInterestCalculator/SimpleInterestCalculatorApplication.java
+++ src/main/java/com/example/simpleInterestCalculator/SimpleInterestCalculatorApplication.java
@@ -2,9 +2,18 @@ package com.example.simpleInterestCalculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SimpleInterestCalculatorApplication {
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleInterestCalculatorApplication.class, args);
--- src/main/java/com/example/simpleInterestCalculator/controller/SimpleInterestController.java
+++ src/main/java/com/example/simpleInterestCalculator/controller/SimpleInterestController.java
@@ -2,20 +2,43 @@ package com.example.simpleInterestCalculator.controller;
 
 import com.example.simpleInterestCalculator.service.SimpleInterestService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/simple-interest")
 public class SimpleInterestController {
 
     @Autowired
+    private RestTemplate restTemplate;
+
+    public SimpleInterestController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Autowired
     private SimpleInterestService simpleInterestService;
 
-    @PostMapping
+    @GetMapping
     public Double calculateSimpleInterest(@RequestParam Double principal, @RequestParam Double rate, @RequestParam Double time) {
         return simpleInterestService.calculateSimpleInterest(principal, rate, time);
     }
+
+    @GetMapping("/compound-interest")
+    public ResponseEntity<Double> getCompoundInterest(@RequestParam double principal, @RequestParam double rate, @RequestParam double time) {
+        double simpleInterest = simpleInterestService.calculateSimpleInterest(principal, rate, time);
+        ResponseEntity<Double> response = restTemplate.getForEntity(
+                "http://sb-service-url/compound-interest?principal={principal}&rate={rate}&time={time}&simpleInterest={simpleInterest}",
+                Double.class, principal, rate, time, simpleInterest);
+        if (response.getStatusCode() == HttpStatus.OK) {
+            return ResponseEntity.ok(response.getBody());
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
 }
